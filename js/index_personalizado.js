b65e5da473af31e2553a8cf090b470c9400369c5
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-05-13 18:14:47 +0100

    Update index_personalizado.js

--- js/index_personalizado.js
+++ js/index_personalizado.js
@@ -13,21 +13,47 @@ firebase.initializeApp(config);
 // Referencia a la base de datos
 var database = firebase.database();
 
-
 $('#login').submit(() => {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+        window.location = "./form.html";
+        // Simulate a mouse click:
+        //window.location.href = "./form.html";
+        // Simulate an HTTP redirect:
+        //window.location.replace("./form.html");
+    })
+    .catch((error) => {
         // Handle Errors here.
-        alert('Error '+error.code+': '+error.message);
+        alert('Error ['+error.code+']: '+error.message);
     });
 
-    // Simulate a mouse click:
-    window.location.href = "./form.html";
-    // Simulate an HTTP redirect:
-    //window.location.replace("./form.html");
-    
+    /*
+    var user = firebase.auth().currentUser;
+    if (user) {
+      alert(user.email+' is logged in.');
+    } else {
+        alert('No user is logged in.');
+    }
+    */
     // Para cancelar el submit
     return false;
 });
+
+
+/*
+{
+  "rules": {
+    "libros": {
+    	"$libro": {
+      	".read": "auth != null",
+        ".write": "auth != null",
+        // Comprueba que todas las entradas introducidas en /libros/ tengan los campos: autor, año y título y no estén vacíos
+        ".validate": "newData.child('autor').val() !== '' && newData.child('año').val() !== '' && newData.child('título').val() !== ''"
+      }
+    }
+  }
+}
+*/
